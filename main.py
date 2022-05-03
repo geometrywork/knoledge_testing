df3df651a33cd204fe67546012c4213758308069
geometrywork <96155598+geometrywork@users.noreply.github.com>
2022-05-03 11:07:40 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,187 @@
+from Question import Question
+
+
+question_prompts1 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+question_prompts2 = [
+    "Сколько букв в русском алфавите?\n(a) 22\n(b) 31\n(c) 33\n\n",
+    "Сколько букв в английском алфавите?\n(a) 33\n(b) 26\n(c) 23\n\n",
+    "В каком году был придумал русский алфавит?\n(a) 863\n(b) 1234\n(c) 998\n\n",
+    "Кто придумал русский алфавит?\n(a) Кирилл и Мефодий\n(b) Иван Грозный\n(c) Карл Великий\n\n"
+]
+
+question_prompts3 = [
+    "Корень из 144?\n(a) +/-12\n(b) 12\n(c) 11\n\n",
+    "14 * 15?\n(a) 190\n(b) 210\n(c) 305\n\n",
+    "Число пи равно?\n(a) 3.14\n(b) 3\n(c) 4\n\n",
+    "Как обозначаются рациональные числа?\n(a) R\n(b) Rez\n(c) Q\n\n"
+]
+
+question_prompts4 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+question_prompts5 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+question_prompts6 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+question_prompts7 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+question_prompts8 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+question_prompts9 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+question_prompts10 = [
+    "Какого цвета арбуз?\n(a) Зеленый\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета Банан?\n(a) Желтый\n(b) Красный\n(c) Фиолетовый\n\n",
+    "Какого цвета яблоко?\n(a) Зеленый или красный\n(b) Черный\n(c) Фиолетовый\n\n",
+    "Какого цвета небо?\n(a) Зеленый\n(b) Черный\n(c) Голубой\n\n"
+]
+
+questions1 = [
+    Question(question_prompts1[0], "a"),
+    Question(question_prompts1[1], "a"),
+    Question(question_prompts1[2], "a"),
+    Question(question_prompts1[3], "c")
+]
+
+questions2 = [
+    Question(question_prompts2[0], "c"),
+    Question(question_prompts2[1], "b"),
+    Question(question_prompts2[2], "a"),
+    Question(question_prompts2[3], "a")
+]
+
+questions3 = [
+    Question(question_prompts3[0], "a"),
+    Question(question_prompts3[1], "b"),
+    Question(question_prompts3[2], "a"),
+    Question(question_prompts3[3], "c")
+]
+
+questions4 = [
+    Question(question_prompts4[0], "a"),
+    Question(question_prompts4[1], "a"),
+    Question(question_prompts4[2], "a"),
+    Question(question_prompts4[3], "c")
+]
+
+questions5 = [
+    Question(question_prompts5[0], "a"),
+    Question(question_prompts5[1], "a"),
+    Question(question_prompts5[2], "a"),
+    Question(question_prompts5[3], "c")
+]
+
+questions6 = [
+    Question(question_prompts6[0], "a"),
+    Question(question_prompts6[1], "a"),
+    Question(question_prompts6[2], "a"),
+    Question(question_prompts6[3], "c")
+]
+
+questions7 = [
+    Question(question_prompts7[0], "a"),
+    Question(question_prompts7[1], "a"),
+    Question(question_prompts7[2], "a"),
+    Question(question_prompts7[3], "c")
+]
+
+questions8 = [
+    Question(question_prompts8[0], "a"),
+    Question(question_prompts8[1], "a"),
+    Question(question_prompts8[2], "a"),
+    Question(question_prompts8[3], "c")
+]
+
+questions9 = [
+    Question(question_prompts9[0], "a"),
+    Question(question_prompts9[1], "a"),
+    Question(question_prompts9[2], "a"),
+    Question(question_prompts9[3], "c")
+]
+
+questions10 = [
+    Question(question_prompts10[0], "a"),
+    Question(question_prompts10[1], "a"),
+    Question(question_prompts10[2], "a"),
+    Question(question_prompts10[3], "c")
+]
+
+
+while True:
+    print("1: Тест 1")
+    print("2: Тест 2")
+    print("3: Тест 3")
+    print("4: Тест 4")
+    print("5: Тест 5")
+    print("6: Тест 6")
+    print("7: Тест 7")
+    print("8: Тест 8")
+    print("9: Тест 9")
+    print("10: Тест 10")
+    print("11: Exit")
+
+    ch = int(input("Выберите тест: "))
+    if ch in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11):
+        if (ch == 11):
+            break
+        if (ch == 1):
+            Question.run_test(questions1)
+        elif (ch == 2):
+            Question.run_test(questions2)
+        elif (ch == 3):
+            Question.run_test(questions3)
+        elif (ch == 4):
+            Question.run_test(questions4)
+        elif (ch == 5):
+            Question.run_test(questions5)
+        elif (ch == 6):
+            Question.run_test(questions6)
+        elif (ch == 7):
+            Question.run_test(questions7)
+        elif (ch == 8):
+            Question.run_test(questions8)
+        elif (ch == 9):
+            Question.run_test(questions9)
+        elif (ch == 10):
+            Question.run_test(questions10)
+    else:
+        print("Invalid Input")
+
+
+if __name__ == '__main__':
+    main()
